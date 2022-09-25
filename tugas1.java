5887896cc764bab7cbffb9ba50f03897d6ed32bc
mohazralfathurrazaq <114301881+mohazralfathurrazaq@users.noreply.github.com>
2022-09-25 20:16:05 +0700

    Add files via upload

---
+++ tugas1.java
@@ -0,0 +1,22 @@
+package tugas1;
+import javax.swing.JOptionPane;
+
+// Nama  : Moh Azral Fathurrazaq
+// Kelas : TO21E
+// Nim : 20210040135
+
+public class tugas1 {
+    public static void main(String[] args) {
+    //1
+    JOptionPane.showMessageDialog(null, "Belajar Java");
+    JOptionPane.showMessageDialog(null, "Sangat Mudah Sekali");
+    JOptionPane.showMessageDialog(null, "dan Sangat Menyenangkan");
+    
+    //2
+    String input = JOptionPane.showInputDialog("Matkul apa yang anda pelajari");
+    JOptionPane.showMessageDialog(null, "Belajar" + input + "sangat mudah.");
+        
+               
+    }
+    
+}
